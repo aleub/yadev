f14612f751b1b8a0ce3fcd772cc4865b3de830fe
Andre Leubner <leubner.andre@gmail.com>
2013-05-28 00:48:24 +0200

    fixed timestamps

--- js/blog.js
+++ js/blog.js
@@ -2,6 +2,6 @@
   var times_to_change = $("header > small[data-timestamp]");
 
   $.each(times_to_change, function (idx, el) {
-    el.innerHTML = el.innerHTML.replace('{{timestamp}}', moment(el.getAttribute("data-timestamp")).fromNow());
+    el.innerHTML = el.innerHTML.replace('{{timestamp}}', moment(el.getAttribute("data-timestamp")).subtract('hours', 8).fromNow());
   });
 })(document, window);
